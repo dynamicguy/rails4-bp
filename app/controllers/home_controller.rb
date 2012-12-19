8389434fe62a09b68ec86a0da059d769c5f6c717
Nurul Ferdous <hello@dynamicguy.com>
2012-12-19 18:06:58 +0600

    cleanse

--- app/controllers/home_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    flash.now[:notice] = "Welcome! - love App Scrolls"
-
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = 'f1c5c663f1570de6862051515e00074aeea8f9a7be89957f7dcdbafd792e4d45bbffa574785964654a18fc4580d3e2d090f38dae2c16719c57ea353398f303cf'
