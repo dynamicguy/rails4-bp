c6d5e8297febcb6ac76d9665d232497cf5a397e5
dynamicguy <hello@dynamicguy.com>
2013-10-26 03:50:38 +0600

    prod config updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,12 @@
-set :application, 'rails4bp'
+set :application, 'rails4bp_production'
 set :repo_url, 'git@git.helpdocstest.com:helpiq-search/helpiq-search.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :deploy_to, '/var/www/rails4bp'
+set :stages, %w(staging production)
+set :default_stage, 'production'
+
+set :deploy_to, "/u/apps/#{application}"
 set :scm, :git
 
 set :format, :pretty
@@ -16,15 +19,48 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
 
+set :user, "deploy"
+set :use_sudo, false
+
 namespace :deploy do
+
+  task :start do
+    on roles(:app), in: :sequence, wait: 10 do
+      run "sudo bluepill load /etc/bluepill/#{application}.pill"
+    end
+  end
+
+  task :stop do
+    on roles(:app), in: :sequence, wait: 10 do
+      run "sudo bluepill #{application} stop"
+    end
+  end
+
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:app), in: :sequence, wait: 10 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+      run "sudo bluepill #{application} restart"
     end
   end
+
+  desc "status"
+  task :status do
+    on roles(:app), in: :sequence, wait: 5 do
+      run "sudo bluepill #{application} status"
+    end
+  end
+
+  desc "schema loading"
+  task :schema_load do
+    on roles(:db), in: :groups, limit: 3, wait: 10 do
+      #, :roles => :db, :primary => true do
+      run "cd #{current_path} && rake db:schema:load RAILS_ENV=#{rails_env}"
+    end
+  end
+
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,9 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deployer@helpiq.com}
-role :web, %w{deployer@helpiq.com}
-role :db,  %w{deployer@helpiq.com}
+role :app, %w{deploy@helpiq.com}
+role :web, %w{deploy@helpiq.com}
+role :db,  %w{deploy@helpiq.com}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +15,7 @@ role :db,  %w{deployer@helpiq.com}
 # definition into the server list. The second argument
 # something that quacks like a has can be used to set
 # extended properties on the server.
-server '162.209.48.170', user: 'deployer', roles: %w{web app}, my_property: :my_value
+server '162.209.48.170', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
