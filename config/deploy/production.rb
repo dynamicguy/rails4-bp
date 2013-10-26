c87e19b6a13e408a40bec00ff699223a35e23f9b
dynamicguy <hello@dynamicguy.com>
2013-10-27 02:04:28 +0600

    cap deploy fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,6 @@
-set :rails_env, :production
+set :rails_env, "production"
+
+set :application, '162.209.48.170'
+role :app, application
+role :web, application
+role :db, application, :primary => true
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,5 @@
-set :rails_env, :staging
+set :rails_env, "staging"
+set :application, '162.209.48.170'
+role :app, application
+role :web, application
+role :db,  application, :primary => true
